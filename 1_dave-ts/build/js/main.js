9cec93bbbc2627a1b8b6bc48608274e9c08fd7f4
omeatai <omeatai@gmail.com>
2024-01-31 23:39:29 -0700

    2-Basic Types

--- 1_dave-ts/build/js/main.js
+++ 1_dave-ts/build/js/main.js
@@ -1,5 +1,17 @@
 "use strict";
-let username = "Hope Guild";
-console.log(username);
+let username;
+let password;
+let deskNumber;
+let isAdmin;
+let isActive;
+let nickname;
+let postId;
+username = "Hope Guild";
+deskNumber = 6;
+isAdmin = false;
+const re = /\w+/g;
+const sum = (a, b) => {
+    return a + b;
+};
 const userNode = document.getElementById("userNode");
 userNode ? (userNode.innerHTML = username) : null;
--- 1_dave-ts/src/main.ts
+++ 1_dave-ts/src/main.ts
@@ -1,6 +1,20 @@
-let username: string = "Hope Guild";
-console.log(username);
+let username: string;
+let password: string;
+let deskNumber: number;
+let isAdmin: boolean;
+let isActive: number | boolean | string;
+let nickname: any;
+let postId: string | number;
 
-const userNode = document.getElementById("userNode");
+username = "Hope Guild";
+deskNumber = 6;
+isAdmin = false;
+const re: RegExp = /\w+/g;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+const userNode: HTMLElement | null = document.getElementById("userNode");
 
 userNode ? (userNode.innerHTML = username) : null;
