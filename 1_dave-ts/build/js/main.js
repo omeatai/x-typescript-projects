af10970d4e297e4d670fa529abf3ed4cf322716f
omeatai <omeatai@gmail.com>
2024-02-03 17:17:57 -0700

    5. Using Literal Types
    5. Using Literal Types

--- 1_dave-ts/build/js/main.js
+++ 1_dave-ts/build/js/main.js
@@ -1,2 +1,4 @@
 "use strict";
-// Type Aliases
+// Literal types
+let userName;
+userName = "Amy";
--- 1_dave-ts/src/main.ts
+++ 1_dave-ts/src/main.ts
@@ -1,12 +1,4 @@
-// Type Aliases
+// Literal types
 
-type stringOrNumber = string | number;
-
-type stringOrNumberArray = (string | number)[];
-type stringOrNumberArray2 = Array<string | number>;
-
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringOrNumberArray;
-};
+let userName: "Dave" | "John" | "Amy";
+userName = "Amy";
