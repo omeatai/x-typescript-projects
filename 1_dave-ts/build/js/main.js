48342c249cc66dcc3b68c2ca9a3e4f3c8a19d56e
omeatai <omeatai@gmail.com>
2024-02-03 18:16:54 -0700

    Using Functions
    Using Functions

--- 1_dave-ts/build/js/main.js
+++ 1_dave-ts/build/js/main.js
@@ -1,4 +1,36 @@
 "use strict";
-// Literal types
-let userName;
-userName = "Amy";
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("Hello!");
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+// default parameters
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+// Rest Parameters
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4, 5));
--- 1_dave-ts/src/main.ts
+++ 1_dave-ts/src/main.ts
@@ -1,4 +1,54 @@
-// Literal types
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
 
-let userName: "Dave" | "John" | "Amy";
-userName = "Amy";
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("Hello!");
+logMsg(add(2, 3));
+
+// Types and Interfaces with functions
+
+type mathFunction = (a: number, b: number) => number;
+
+interface mathFunction2 {
+  (a: number, b: number): number;
+}
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// default parameters
+
+const sumAll = (a: number, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+
+// Rest Parameters
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((prev, curr) => prev + curr);
+};
+
+logMsg(total(1, 2, 3, 4, 5));
