199dd9b10356c9cf65cfbc4639046818adfba251
omeatai <omeatai@gmail.com>
2024-02-04 20:02:17 -0700

    15. Vite.js with Typescript
    15. Vite.js with Typescript

--- 1_dave-ts/build/js/main.js
+++ 1_dave-ts/build/js/main.js
@@ -1,123 +1,74 @@
 "use strict";
-const echo = (arg) => arg;
-const isObj = (arg) => {
-    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+// Utility Types
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-console.log(isObj(true));
-console.log(isObj("John"));
-console.log(isObj([1, 2, 3]));
-console.log(isObj({ name: "John" }));
-console.log(isObj(null));
-console.log(isObj(undefined));
-console.log(isObj(1));
-console.log(isObj(NaN));
-const isTrue = (arg) => {
-    if (Array.isArray(arg) && !arg.length) {
-        return { arg, is: false };
-    }
-    if (isObj(arg) && !Object.keys(arg).length) {
-        return { arg, is: false };
-    }
-    return { arg, is: !!arg };
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
-console.log(isTrue(false));
-console.log(isTrue(0));
-console.log(isTrue(true));
-console.log(isTrue(1));
-console.log(isTrue("Dave"));
-console.log(isTrue(""));
-console.log(isTrue(null));
-console.log(isTrue(undefined));
-console.log(isTrue({})); // modified
-console.log(isTrue({ name: "Dave" }));
-console.log(isTrue([])); // modified
-console.log(isTrue([1, 2, 3]));
-console.log(isTrue(NaN));
-console.log(isTrue(-0));
-const checkBoolValue = (arg) => {
-    if (Array.isArray(arg) && !arg.length) {
-        return { value: arg, is: false };
-    }
-    if (isObj(arg) && !Object.keys(arg).length) {
-        return { value: arg, is: false };
-    }
-    return { value: arg, is: !!arg };
+const assign1 = {
+    studentId: "compsci123",
+    title: "Final Project",
+    grade: 0,
 };
-const processUser = (user) => {
-    // process the user with logic here
-    return user;
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+// Required and Readonly
+const recordAssignment = (assign) => {
+    // send to database, etc.
+    return assign;
 };
-console.log(processUser({ id: 1, name: "Dave" }));
-///////////////////////////////////////
-const getUsersProperty = (users, key) => {
-    return users.map((user) => user[key]);
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// NOTE: assignVerified won't work with recordAssignment!
+// Why? Try it and see what TS tells you :)
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// Record
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
 };
-const usersArray = [
-    {
-        id: 1,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-                lat: "-37.3159",
-                lng: "81.1496",
-            },
-        },
-        phone: "1-770-736-8031 x56442",
-        website: "hildegard.org",
-        company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets",
-        },
-    },
-    {
-        id: 2,
-        name: "Ervin Howell",
-        username: "Antonette",
-        email: "Shanna@melissa.tv",
-        address: {
-            street: "Victor Plains",
-            suite: "Suite 879",
-            city: "Wisokyburgh",
-            zipcode: "90566-7771",
-            geo: {
-                lat: "-43.9509",
-                lng: "-34.4618",
-            },
-        },
-        phone: "010-692-6593 x09125",
-        website: "anastasia.net",
-        company: {
-            name: "Deckow-Crist",
-            catchPhrase: "Proactive didactic contingency",
-            bs: "synergize scalable supply-chains",
-        },
-    },
-];
-console.log(getUsersProperty(usersArray, "email"));
-console.log(getUsersProperty(usersArray, "username"));
-///////////////////////////////////////
-class StateObject {
-    constructor(value) {
-        this.data = value;
-    }
-    get state() {
-        return this.data;
-    }
-    set state(value) {
-        this.data = value;
-    }
-}
-const store = new StateObject("John");
-console.log(store.state);
-store.state = "Dave";
-//store.state = 12
-const myState = new StateObject([15]);
-myState.state = ["Dave", 42, true];
-console.log(myState.state);
+const finalGrades = {
+    Sara: "B",
+    Kelly: "U",
+};
+const gradeData = {
+    Sara: { assign1: 85, assign2: 93 },
+    Kelly: { assign1: 76, assign2: 15 },
+};
+const score = {
+    studentId: "k123",
+    grade: 85,
+};
+const preview = {
+    studentId: "k123",
+    title: "Final Project",
+};
+// ReturnType
+//type newAssign = { title: string, points: number }
+const createNewAssign = (title, points) => {
+    return { title, points };
+};
+const tsAssign = createNewAssign("Utility Types", 100);
+console.log(tsAssign);
+const assignArgs = ["Generics", 100];
+const tsAssign2 = createNewAssign(...assignArgs);
+console.log(tsAssign2);
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+        return res.json();
+    })
+        .catch((err) => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+fetchUsers().then((users) => console.log(users));
--- 1_dave-ts/src/main.ts
+++ 1_dave-ts/src/main.ts
@@ -1,156 +1,141 @@
-const echo = <T>(arg: T): T => arg;
+// Utility Types
 
-const isObj = <T>(arg: T): boolean => {
-  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
-};
+// Partial
 
-console.log(isObj(true));
-console.log(isObj("John"));
-console.log(isObj([1, 2, 3]));
-console.log(isObj({ name: "John" }));
-console.log(isObj(null));
-console.log(isObj(undefined));
-console.log(isObj(1));
-console.log(isObj(NaN));
-
-const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
-  if (Array.isArray(arg) && !arg.length) {
-    return { arg, is: false };
-  }
-  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
-    return { arg, is: false };
-  }
-  return { arg, is: !!arg };
-};
-
-console.log(isTrue(false));
-console.log(isTrue(0));
-console.log(isTrue(true));
-console.log(isTrue(1));
-console.log(isTrue("Dave"));
-console.log(isTrue(""));
-console.log(isTrue(null));
-console.log(isTrue(undefined));
-console.log(isTrue({})); // modified
-console.log(isTrue({ name: "Dave" }));
-console.log(isTrue([])); // modified
-console.log(isTrue([1, 2, 3]));
-console.log(isTrue(NaN));
-console.log(isTrue(-0));
-
-//////////////////////////////////////
-
-interface BoolCheck<T> {
-  value: T;
-  is: boolean;
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
 }
 
-const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
-  if (Array.isArray(arg) && !arg.length) {
-    return { value: arg, is: false };
-  }
-  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
-    return { value: arg, is: false };
-  }
-  return { value: arg, is: !!arg };
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...propsToUpdate };
 };
 
-//////////////////////////////////////
+const assign1: Assignment = {
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
+};
 
-interface HasID {
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
+
+// Required and Readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // send to database, etc.
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+// NOTE: assignVerified won't work with recordAssignment!
+// Why? Try it and see what TS tells you :)
+
+recordAssignment({ ...assignGraded, verified: true });
+
+// Record
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
+};
+
+type Students = "Sara" | "Kelly";
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "U",
+};
+
+interface Grades {
+  assign1: number;
+  assign2: number;
+}
+
+const gradeData: Record<Students, Grades> = {
+  Sara: { assign1: 85, assign2: 93 },
+  Kelly: { assign1: 76, assign2: 15 },
+};
+
+// Pick and Omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">;
+
+const score: AssignResult = {
+  studentId: "k123",
+  grade: 85,
+};
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">;
+
+const preview: AssignPreview = {
+  studentId: "k123",
+  title: "Final Project",
+};
+
+// Exclude and Extract
+
+type adjustedGrade = Exclude<LetterGrades, "U">;
+
+type highGrades = Extract<LetterGrades, "A" | "B">;
+
+// Nonnullable
+
+type AllPossibleGrades = "Dave" | "John" | null | undefined;
+type NamesOnly = NonNullable<AllPossibleGrades>;
+
+// ReturnType
+
+//type newAssign = { title: string, points: number }
+
+const createNewAssign = (title: string, points: number) => {
+  return { title, points };
+};
+
+type NewAssign = ReturnType<typeof createNewAssign>;
+
+const tsAssign: NewAssign = createNewAssign("Utility Types", 100);
+console.log(tsAssign);
+
+// Parameters
+
+type AssignParams = Parameters<typeof createNewAssign>;
+
+const assignArgs: AssignParams = ["Generics", 100];
+
+const tsAssign2: NewAssign = createNewAssign(...assignArgs);
+console.log(tsAssign2);
+
+// Awaited - helps us with the ReturnType of a Promise
+
+interface User {
   id: number;
-}
-const processUser = <T extends HasID>(user: T): T => {
-  // process the user with logic here
-  return user;
-};
-
-console.log(processUser({ id: 1, name: "Dave" }));
-
-///////////////////////////////////////
-
-const getUsersProperty = <T extends HasID, K extends keyof T>(
-  users: T[],
-  key: K
-): T[K][] => {
-  return users.map((user) => user[key]);
-};
-
-const usersArray = [
-  {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-      street: "Kulas Light",
-      suite: "Apt. 556",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      geo: {
-        lat: "-37.3159",
-        lng: "81.1496",
-      },
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    company: {
-      name: "Romaguera-Crona",
-      catchPhrase: "Multi-layered client-server neural-net",
-      bs: "harness real-time e-markets",
-    },
-  },
-  {
-    id: 2,
-    name: "Ervin Howell",
-    username: "Antonette",
-    email: "Shanna@melissa.tv",
-    address: {
-      street: "Victor Plains",
-      suite: "Suite 879",
-      city: "Wisokyburgh",
-      zipcode: "90566-7771",
-      geo: {
-        lat: "-43.9509",
-        lng: "-34.4618",
-      },
-    },
-    phone: "010-692-6593 x09125",
-    website: "anastasia.net",
-    company: {
-      name: "Deckow-Crist",
-      catchPhrase: "Proactive didactic contingency",
-      bs: "synergize scalable supply-chains",
-    },
-  },
-];
-
-console.log(getUsersProperty(usersArray, "email"));
-console.log(getUsersProperty(usersArray, "username"));
-
-///////////////////////////////////////
-
-class StateObject<T> {
-  private data: T;
-
-  constructor(value: T) {
-    this.data = value;
-  }
-
-  get state(): T {
-    return this.data;
-  }
-
-  set state(value: T) {
-    this.data = value;
-  }
+  name: string;
+  username: string;
+  email: string;
 }
 
-const store = new StateObject("John");
-console.log(store.state);
-store.state = "Dave";
-//store.state = 12
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      if (err instanceof Error) console.log(err.message);
+    });
+  return data;
+};
 
-const myState = new StateObject<(string | number | boolean)[]>([15]);
-myState.state = ["Dave", 42, true];
-console.log(myState.state);
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users) => console.log(users));
